c3e46f6a805fba3993381c920c44ffb9fab0b391
Nir Sultan <snufkin62@gmail.com>
2025-04-14 23:41:37 +0300

    Fix img file path

--- cmps/BookPreview.jsx
+++ cmps/BookPreview.jsx
@@ -1,11 +1,13 @@
 import { formatCurrency } from "../services/util.service.js"
+import saleImg from '../assets/img/sale.png'
 
 export function BookPreview({ book }) {
 
   const { title, listPrice, thumbnail } = book
 
   function getSaleTag() {
-    if (listPrice.isOnSale) return <img className="sale" src="../assets/img/sale.png" alt="" />
+    // if (listPrice.isOnSale) return <img className="sale" src="../assets/img/sale.png" alt="" />
+    if (listPrice.isOnSale) return <img className="sale" src={saleImg} alt="" />
     return null
   }
 
