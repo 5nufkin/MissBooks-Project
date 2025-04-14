a111900ecbf1956deae1faca6c8530d158ded209
Nir Sultan <snufkin62@gmail.com>
2025-04-14 19:10:09 +0300

    Add book details features

--- cmps/BookPreview.jsx
+++ cmps/BookPreview.jsx
@@ -4,8 +4,14 @@ export function BookPreview({ book }) {
 
   const { title, listPrice, thumbnail } = book
 
+  function getSaleTag() {
+    if (listPrice.isOnSale) return <img className="sale" src="../assets/img/sale.png" alt="" />
+    return null
+  }
+
   return (
     <article className="book-preview">
+      {getSaleTag()}
       <h2>Title: {title}</h2>
       <h4>Book Price: {formatCurrency(listPrice.amount, listPrice.currencyCode)} </h4>
       <img src={`${thumbnail}`} alt="Book Image" />
--- pages/BookDetails.jsx
+++ pages/BookDetails.jsx
@@ -18,11 +18,31 @@ export function BookDetails({ bookId, onBack }) {
       .catch(err => console.log('err:', err))
   }
 
-  function getReadLevelTxt() {
+  function getReadLevelTag() {
     const pageCount = book.pageCount
     if (pageCount >= 500) return 'Serious Reading'
     if (pageCount >= 200) return 'Descent Reading'
     return 'Light Reading'
+  }
+
+  function getBookAgeTag() {
+    const publishYear = book.publishedDate
+    const currYear = new Date().getFullYear()
+    if (currYear - publishYear > 10) return 'Vintage'
+    if (currYear - publishYear <= 1) return 'New'
+    return null
+  }
+
+  function getPriceColorClass() {
+    const price = book.listPrice.amount
+    if (price > 150) return 'red'
+    if (price < 20) return 'green'
+    return ''
+  }
+
+  function getSaleSticker() {
+    if (book.listPrice.isOnSale) return <img className="sale-sticker" src="../assets/img/saleSticker.png" alt="" />
+    return null
   }
 
   if (!book) return <div>Loading...</div>
@@ -32,11 +52,16 @@ export function BookDetails({ bookId, onBack }) {
 
       <h1>Book Title: {title}</h1>
       <section className="tags">
-        <span>{getReadLevelTxt()}</span>
+        <span>{getReadLevelTag()}</span>
+        <span>{getBookAgeTag()}</span>
+
       </section>
-      <h1>Book Price: {formatCurrency(listPrice.amount, listPrice.currencyCode)} </h1>
+      <h1>Book Price: <span className={`price ${getPriceColorClass()}`}>{formatCurrency(listPrice.amount, listPrice.currencyCode)} </span></h1>
       <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis quae fuga eveniet, quisquam ducimus modi optio in alias accusantium corrupti veritatis commodi tenetur voluptate deserunt nihil quibusdam. Expedita, architecto omnis?</p>
-      <img src={`${thumbnail}`} alt="Book Image" />
+      <div className="img-container">
+        <img src={`${thumbnail}`} alt="Book Image" />
+        {getSaleSticker()}
+      </div>
       <button onClick={onBack}>Back</button>
 
     </section>
